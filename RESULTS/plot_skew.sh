d0d4cae3743d689a7a3727ea22ab6ba5b38a26b3
devekar <vaibhav.devekar@gmail.com>
2013-11-11 21:07:18 -0500

    same means

--- RESULTS/plot_skew.sh
+++ RESULTS/plot_skew.sh
@@ -4,8 +4,7 @@ set xlabel "Number of clusters"
 set terminal png size 800,600 enhanced font "Helvetica,20"
 set output "Kmeans-skew.png"
 
-plot "skew_mean_sd.txt" using 1:2 title 'Means(Cosine)' with lines,\
-     "skew_mean_sd.txt" using 1:4 title 'Means(Euclidean)' with lines,\
+plot "skew_mean_sd.txt" using 1:2 title 'Means' with lines,\
      "skew_mean_sd.txt" using 1:3 title 'Std-Dev(Cosine)' with lines,\
      "skew_mean_sd.txt" using 1:5 title 'Std-Dev(Euclidean)' with lines
 
@@ -16,7 +15,6 @@ set xlabel "Number of clusters"
 set terminal png size 800,600 enhanced font "Helvetica,20"
 set output "Maxlink-skew.png"
 
-plot "maxlink_mean_sd.txt" using 1:2 title 'Means(Cosine)' with lines,\
-     "maxlink_mean_sd.txt" using 1:4 title 'Means(Euclidean)' with lines,\
+plot "maxlink_mean_sd.txt" using 1:2 title 'Means' with lines,\
      "maxlink_mean_sd.txt" using 1:3 title 'Std-Dev(Cosine)' with lines,\
      "maxlink_mean_sd.txt" using 1:5 title 'Std-Dev(Euclidean)' with lines
